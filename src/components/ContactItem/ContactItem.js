704fa580127a1b52738e900d2856a083314c20a0
sonyaaat <sofiatkach2004@gmail.com>
2022-11-14 00:23:25 +0200

    Правки 2

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -2,7 +2,7 @@ import css from "../ContactItem/ContactItem.module.css"
 import PropTypes from 'prop-types';
 const ContactItem=({id,name,number,onDelete})=>{
 return(
-    <li className={css.item} key={id}>
+    <li className={css.item}>
          <p className={css.name}>{name}: </p>
          <p className={css.number}>{number}</p>
          <button className={css.button} type="button" onClick={()=>onDelete(id)}>Delete</button>
